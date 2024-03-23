262c558ccf67aa9e0fced791272b174df7e69659
Vova Romanyuck <vovaromanyuck@MacBook-Air-Vova.local>
2024-03-23 10:14:58 +0200

    features/auth-backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dima123:TZUDbx6cCDWya8zq@cluster0.4r4ikxy.mongodb.net/',
+    ),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
